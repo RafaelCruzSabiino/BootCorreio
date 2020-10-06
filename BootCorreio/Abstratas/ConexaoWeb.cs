a30f79470a0f74396725c6e6681bf55b63a17747
RafaelCruzSabiino <72409805+RafaelCruzSabiino@users.noreply.github.com>
2020-10-06 08:56:06 -0300

    Add files via upload

---
+++ BootCorreio/Abstratas/ConexaoWeb.cs
@@ -0,0 +1,48 @@
+﻿using BootCorreio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCorreio.Abstratas
+{
+    public abstract class ConexaoWeb
+    {
+        //private Correios _webcorreio;
+
+        public ConexaoWeb()
+        {
+        }
+
+        public InfoEndereco GetEndereco(string cep)
+        {
+            InfoEndereco result = new InfoEndereco();
+
+            try
+            {
+                using (var WebCorreio = new Correios.AtendeClienteClient())
+                {
+                    var resposta = WebCorreio.consultaCEP(cep);
+
+                    result.Logradouro   = ValidarCampo(resposta.end);
+                    result.Bairro       = ValidarCampo(resposta.bairro);
+                    result.Cidade       = ValidarCampo(resposta.cidade);
+                    result.Estado       = ValidarCampo(resposta.uf);
+                    result.CEP          = ValidarCampo(resposta.cep);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+
+            return result;
+        }
+
+        private string ValidarCampo(string str)
+        {
+            return string.IsNullOrEmpty(str) ? string.Empty : str.Trim();
+        }
+    }
+}
---
+++ BootCorreio/Modelos/InfoEndereco.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCorreio.Modelos
+{
+    public class InfoEndereco
+    {
+        public string CEP           { get; set; }
+        public string Logradouro    { get; set; }
+        public string Bairro        { get; set; }
+        public string Cidade        { get; set; }
+        public string Estado        { get; set; }
+
+        public List<string> ListaCeps;
+
+        public InfoEndereco()
+        {
+            CEP         = string.Empty;
+            Logradouro  = string.Empty;
+            Bairro      = string.Empty;
+            Cidade      = string.Empty;
+            Estado      = string.Empty;
+            ListaCeps   = new List<string>();
+        }
+
+        public List<string> GerarListaCeps()
+        {
+            ListaCeps.Add("17213540");
+            ListaCeps.Add("17213530");
+            ListaCeps.Add("17213520");
+            ListaCeps.Add("17213570");
+            ListaCeps.Add("17213580");
+
+            return ListaCeps;
+        }
+    }
+}
