f9bc2e51b89e8eeec7f262f7d8b0dc073d56548e
RafaelCruzSabiino <72409805+RafaelCruzSabiino@users.noreply.github.com>
2020-10-06 09:00:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using BootCorreio.Robo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCorreio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boot = new Boot();
+
+            boot.Start();
+        }
+    }
+}
